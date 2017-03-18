2b0b3c803849f0899bc4ce7c21d180cd23e40c4d
Jerry <jerrydwalters@gmail.com>
2017-03-18 15:30:33 -0400

    removed some junk

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import '../styles/App.css';
 
 import TestContainer from '../containers/TestContainer';
-import DevTools from '../containers/DevTools';
 
 const App = () => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
     <TestContainer />
-    <DevTools />
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './store'
 import App from './components/App';
-import './index.css';
+import './styles/index.css';
 
 const store = configureStore();
 
